a815e92197020d77c780813382382102405176c7
Thien Tran <fcduythien@gmail.com>
2018-09-17 10:53:02 +0700

    Update dev version

--- dev/mongodb/main.tf
+++ dev/mongodb/main.tf
@@ -1,23 +1,24 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-staging-urbn8.io"
-    key    = "mongodb/terraform-dev.tfstate"
+    bucket = "terraform-dev-urbn8.io"
+    key    = "mongodb/terraform.tfstate"
     region = "eu-central-1"
   }
 }
 
 module "mongodb" {
-  source      = "../../modules/mongodb"
-  environment = "dev"
-  domain      = "urbn8.io"
-  vpc_id      = "vpc-efc99484"
-  dns_zone    = "Z14R93S7FNVUY1"
+  source                 = "../../modules/mongodb"
+  environment            = "dev"
+  domain                 = "urbn8.io"
+  vpc_id                 = "vpc-efc99484"
+  dns_zone               = "Z14R93S7FNVUY1"
+  ami_id                 = "ami-0adbe2a35a4b9fb93"
+  vpc_security_group_ids = ["sg-a77cd1cb", "sg-014ce16d", "sg-3871dc54", "sg-05ec5a4c8be661b91"]
 
-  # Allow each node can access mongodb
-  bastion_security_group = "sg-3871dc54"
-  key_name               = "frankfurt"
-  private_subnets        = ["subnet-2b616140", "subnet-4b9de236", "subnet-82216ccf"]
-  vol_size               = "10"
+  instance_type = "i3.large"
+  key_name      = "frankfurt"
+  subnet_ids    = ["subnet-c35f5fa8", "subnet-aea5dad3", "subnet-77115c3a"]
+  vol_size      = "10"
 }
 
 module "slack" {
